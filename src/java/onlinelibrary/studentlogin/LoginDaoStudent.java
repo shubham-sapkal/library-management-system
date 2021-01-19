ff108d97874a65d74cd88bd5b1e7ef8e80a84e3f
shubham-sapkal <sapkal.shubham2002@gmail.com>
2021-01-19 20:38:28 +0530

    Half student login done

--- src/java/onlinelibrary/studentlogin/LoginDaoStudent.java
+++ src/java/onlinelibrary/studentlogin/LoginDaoStudent.java
@@ -11,33 +11,37 @@ import onlinelibrary.data.DBUtil;
 import onlinelibrary.util.PasswordUtil;
 
 public class LoginDaoStudent {
-	public static String getName(String username,String hashedpwd) {
+    
+	public static String getName(String username,String password) {
 		ConnectionPool pool = ConnectionPool.getInstance();
 		Connection connection = pool.getConnection();
 		PreparedStatement ps = null;
 		ResultSet rs= null;
 		String name= null;
 		String existingpass;
-		String salt;
-		String newpwd;
-		String query
-		= "SELECT * from Student WHERE username='"+username+"' ;";
+		//String salt;
+		//String newpwd;
+		String query = "SELECT * FROM student WHERE username = ? AND password = ?";
 		try {
-			ps = connection.prepareStatement(query);{
-				rs= ps.executeQuery();
-				while(rs.next()){
-					name= rs.getString("firstname");
-					existingpass= rs.getString("password");
-					salt= rs.getString("salt");
-					try {
-						newpwd= PasswordUtil.hashPassword(hashedpwd + salt);
-					} catch (NoSuchAlgorithmException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-			}
+			ps = connection.prepareStatement(query);
+                        ps.setString(2,username);
+                        ps.setString(1, password);
+                        rs= ps.executeQuery();
+                        while(rs.next()){
+                            name= rs.getString("firstname");
+                            existingpass= rs.getString("password");
+                                            //salt= rs.getString("salt");
+                                            /*
+                                            try {
+                                                    newpwd= PasswordUtil.hashPassword(hashedpwd + salt);
+                                            } catch (NoSuchAlgorithmException e) {
+                                                    // TODO Auto-generated catch block
+                                                    e.printStackTrace();
+                                            }*/
+                            }
+			
 			return name;
+                        
 		} catch (SQLException e) {
 			System.out.println(e);
 			return null;
--- src/java/onlinelibrary/studentlogin/StudentLogin.java
+++ src/java/onlinelibrary/studentlogin/StudentLogin.java
@@ -22,19 +22,10 @@ public class StudentLogin extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException, NullPointerException {
-		String url = "/studentLogin.jsp";
+            
+                        String url = "/studentLogin.jsp";
 
-		// get current action
-		String action = request.getParameter("Action");
-		// get current action
-		String Action = request.getParameter("Action");
-
-		if (Action == null) {
-			Action = "Login";  // default action
-		}
-
-		else if (Action.equals("Login")) {
-			String username=request.getParameter("username");
+                        String username=request.getParameter("username");
 			String pass=request.getParameter("password");
 
                         /*
@@ -59,59 +50,14 @@ public class StudentLogin extends HttpServlet {
 
 				url = "/studentDashboard.jsp";
                                 
-                              
+                              getServletContext().getRequestDispatcher(url).forward(request, response);
+
 			}
 			else {
 				PrintWriter out=response.getWriter();
 				out.println("<script>window.location=\"studentLogin.jsp\";alert(\"Student Username Or Password Is Incorrect ...!\")</script>");
 			}
-			getServletContext()
-			.getRequestDispatcher(url)
-			.forward(request, response);
-
-		}
-
-		else if (Action.equals("view")){
-
-			System.out.println("entered view");
-			String username=request.getParameter("username");
-			String pass=request.getParameter("password");
-			//if(LoginDaoStudent.getName(username, pass) != null) {
-			HttpSession session = request.getSession(true);
-			//			ArrayList<Book> bookslist = new ArrayList<Book>();
-			//			bookslist = BookDB.selectBooks();
-			//			System.out.println("View books are "+bookslist);
-			//			request.setAttribute("book", bookslist);
-			url = "/viewBooks.jsp";
-			//}
-
-			getServletContext()
-			.getRequestDispatcher(url)
-			.forward(request, response);
-
-		}
-
-		else if (Action.equals("issue")){
-
-			System.out.println("entered issue");
-			String username=request.getParameter("username");
-			String pass=request.getParameter("password");
-			//if(LoginDaoStudent.getName(username, pass) != null) {
-			HttpSession session = request.getSession(true);
-			String studentid = (String) session.getAttribute("studentid");
-			System.out.println("student id is "+studentid);
-			ArrayList<Book> issuelist= new ArrayList<Book>();
-			issuelist=	IssueDB.issuedBooks(studentid);
-			request.setAttribute("book", issuelist);
-			System.out.println("Issued books are "+issuelist);
-			url = "/viewIssuedBooks.jsp";
-			//}
-			getServletContext()
-			.getRequestDispatcher(url)
-			.forward(request, response);
-
-		}
-
+			
 	}
 
 }
